4aca1e247d1bc8d13aaeca1f60bcc8e4ddd87e4c
Maksim <ignatov.maksim.04@mail.ru>
2023-12-20 19:47:30 +0300

    Final commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Application.Extensions;
+using Infrastructure.DataAccess.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Presentation.Console;
+using Presentation.Console.Extensions;
+using Spectre.Console;
+
+var collection = new ServiceCollection();
+
+collection
+    .AddApplication()
+    .AddInfrastructureDataAccess(configuration =>
+    {
+        configuration.Host = "localhost";
+        configuration.Port = 6432;
+        configuration.Username = "postgres";
+        configuration.Password = "postgres";
+        configuration.Database = "postgres";
+        configuration.SslMode = "Prefer";
+    })
+    .AddPresentationConsole();
+
+ServiceProvider provider = collection.BuildServiceProvider();
+using IServiceScope scope = provider.CreateScope();
+
+scope.UseInfrastructureDataAccess();
+
+ScenarioRunner scenarioRunner = scope.ServiceProvider
+    .GetRequiredService<ScenarioRunner>();
+
+while (true)
+{
+    scenarioRunner.Run();
+    AnsiConsole.Clear();
+}
